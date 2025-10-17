4dbf9867bb0b698cbfe52476858e506a559b58ac
Adriana Tint <adrianasaroux@yahoo.com>
2025-10-17 15:42:29 +0200

    Correction ddes alertes streamlit

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,7 +1,7 @@
-import streamlit as st
-import requests
-import pandas as pd
-import matplotlib.pyplot as plt
+import streamlit as st # type: ignore
+import requests # type: ignore
+import pandas as pd # type: ignore
+import matplotlib.pyplot as plt # type: ignore
 import base64
 import io
 
